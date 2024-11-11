194038a4464a259c8da96b4e3c751775e1c55e0e
Nima Ghadamli <162907223+MrNimaG@users.noreply.github.com>
2024-11-10 20:54:28 -0500

    Update Project 9 - Blind Audition Bidding.py

--- Project 9 - Blind Audition Bidding.py
+++ Project 9 - Blind Audition Bidding.py
@@ -2,7 +2,7 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
-import art
+# import art
 print(art.logo)
 
 bidders ={}
